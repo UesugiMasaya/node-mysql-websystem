c15d70f4c86ccd05a2db142193b1113dd050cdfe
UesugiMasaya <j2307@matsue-ct.ac.jp>
2026-08-17 09:09:12 +0000

    Chapter13 display tasks from database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.query(
+    'SELECT * FROM tasks',
+    function(error, results) {
+      res.render('index', { tasks: results });
+    }
+  );
 });
 
 module.exports = router;
