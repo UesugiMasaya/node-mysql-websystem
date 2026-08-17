c5d0beae89455016d38c64af5f2c87ddd275e846
UesugiMasaya <j2307@matsue-ct.ac.jp>
2026-08-17 14:24:49 +0000

    Chapter19 completed

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,17 @@ var router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', async function(req, res, next) {
-  const tasks = await knex('tasks').select('*');
+  const tasks = await knex('tasks')
+  .where({ user_id: req.user.id })
+  .select('*');
+
   res.render('index', { tasks: tasks });
 });
 router.post('/create', async function(req, res, next) {
   const content = req.body.content;
 
   await knex('tasks').insert({
-    user_id: 1,
+    user_id: req.user.id,
     content: content
   });
 
